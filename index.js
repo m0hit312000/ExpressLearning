b4fbc199fa1f1d6feeb02d7ef85231d1682ef061
Mohit <mm8085574989@gmail.com>
2020-08-12 11:13:50 +0530

    Express Router

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -10,28 +11,10 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-//  ***************************************************************************************************************
 
-app.all('/dishes',(req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next(); // Look for other methods of app for dishes
-});
-app.get('/dishes',(req,res,next) => {
-    res.end('Will send all the dishes to you!');
-});
-app.post('/dishes',(req,res,next) => {
-    res.end('Will add the dish: ' + req.body.name + ' with details ' + req.body.description);
-});
-app.put('/dishes',(req,res,next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on dishes');
-});
-app.delete('/dishes',(req,res,next) => {
-    res.end('Deleting all the dishes!');
-});
+app.use('/dishes', dishRouter);
+app.use(express.static(__dirname + '/public'));
 
-//  ***************************************************************************************************************
 
 app.all('/dishes/:dishId',(req,res,next) => {
     res.statusCode = 200;
@@ -55,7 +38,6 @@ app.delete('/dishes/:dishId',(req,res,next) => {
 
 // ******************************************************************************************************************
 
-app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
     res.statusCode = 200;
