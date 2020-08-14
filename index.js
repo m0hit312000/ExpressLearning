6450a27437b81647f037612d69fb71132ce0ae13
Mohit <mm8085574989@gmail.com>
2020-08-14 07:07:04 +0530

    Assignment 1

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -13,31 +15,10 @@ app.use(bodyParser.json());
 
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+
 app.use(express.static(__dirname + '/public'));
-
-
-app.all('/dishes/:dishId',(req,res,next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    next(); // Look for other methods of app for dishes
-});
-app.get('/dishes/:dishId',(req,res,next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-});
-app.post('/dishes/:dishId',(req,res,next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on dishes/ ' + req.params.dishId);
-});
-app.put('/dishes/:dishId',(req,res,next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will Update the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-app.delete('/dishes/:dishId',(req,res,next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
-
-// ******************************************************************************************************************
-
 
 app.use((req, res, next) => {
     res.statusCode = 200;
